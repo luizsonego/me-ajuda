0772f7ebb3618bcb5b4a0af7de570e75a848d82c
Luiz Yavorski <leesonego@gmail.com>
2020-09-21 12:56:32 -0300

    update composer

---
+++ console/migrations/m200918_015826_create_table_improvement.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200918_015826_create_table_improvement
+ */
+class m200918_015826_create_table_improvement extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200918_015826_create_table_improvement cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200918_015826_create_table_improvement cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
